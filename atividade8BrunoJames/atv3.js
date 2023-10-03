9d3eea1dedf30d38a990f61058c176522459dbbd
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2023-10-03 10:24:29 -0300

    att4

--- atividade8BrunoJames/atv3.js
+++ atividade8BrunoJames/atv3.js
@@ -1,1 +1,36 @@
+const leitor = require('readline-sync')
 
+let valor1 = leitor.questionInt("Ensira um valor: ")
+let valor2 = leitor.questionInt("Ensira outro valor: ")
+
+function soma(a,b) {
+    let som = a + b
+    console.log(som);
+}
+
+function sub(a,b) {
+    let su = a - b
+    console.log(su);
+}
+
+function mult(a,b) {
+    let mul = a*b
+    console.log(mul);
+}
+
+function div(a,b) {
+    let di = a / b
+    console.log(di);
+}
+
+function main() {
+    console.log(`Numero inseridos ${valor1} e ${valor2}`);
+    console.log("Soma: ", soma(valor1,valor2));
+    console.log("Subtracao: ",sub(valor1,valor2));
+    console.log("Multiplicacao: ",mult(valor1,valor2));
+    console.log("Divisao: ",div(valor1,valor2));
+}
+
+
+
+main()
---
+++ atividade8BrunoJames/desafio.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 20
+
+let mais = a+b
+
+const arrow3 = (a) => console.log(a);
+    
+const arrow2 = (a,b) => arrow3(mais)
+arrow2(a,b)
