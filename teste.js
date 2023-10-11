29e44ed22511ba60e7c75efe2dff5ae930d11106
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2023-10-11 11:48:40 -0300

    att7

--- teste.js
+++ teste.js
@@ -1,10 +1,17 @@
 
-let a = 10  
-let b = 2
 
-if (a == b) {
-    console.log(true);
-    
-}else{
-    return false
-}
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let a
+let b
+let soma
+rl.question(``, a => {
+    rl.question(``, b => {
+        soma = Number(a) + Number(b)
+        console.log(`Soma = ${soma}`);
+    })
+})
