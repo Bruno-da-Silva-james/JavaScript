25bb390db173b1519390314d493be03b6bd5d216
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2023-11-01 11:51:58 -0300

    Update TrabalhoTurmas.js

--- trabalho4BrunoJames/TrabalhoTurmas.js
+++ trabalho4BrunoJames/TrabalhoTurmas.js
@@ -1,34 +1,57 @@
 const leitor = require('readline-sync')
-
+    let alunoL = {}
+    let alunos = []
+    let arrayMaior = []
 function main() {
-    let qntAluno = leitor.questionInt("Ensira a quantidade de alunos: ")
-    getNota(qntAluno, 0)
+    login()
 }
 
 function login() {
-    let nomeProf = leitor.questionInt("Ensira seu nome: ")
-    let nomeMat = leitor.questionInt("Ensira a materia que leciona: ")
+    let aluno = []
+    let nomeProf = leitor.question("\nEnsira seu nome: ")
+    let nomeMat = leitor.question("\nEnsira a materia que leciona: ", '\n')
+    console.log(`Ola! ${nomeProf} vamos fazer as medias de ${nomeMat}! \n`);
+    let valAluno = leitor.questionInt('Ensira a quantidade de alunos a serem calculados: ')
+    for (let i = 0; i < valAluno; i++) {
+        alunos.push(teste)
+    }
+    
+    
 
+    console.log(alunos);
+    console.log('As maiores notas sao: ', getNotaMaior());
 }
 
-function getNota(alunos, n) {
-    let alunoL = [{}, {}, {}, {}, {}, {}]
-    while (n < alunos) {
-        let notaT = 0
-        let nota = 0
-        alunoL[n].nome = leitor.question(`Ensira o nome do aluno: `)
-        for (let i = 0; i < 6; i++) {
-            nota = leitor.questionInt(`Ensira a nota do aluno: `)
+function getNotaMaior() {
+    alunos.forEach(a => {
+        arrayMaior.push(a.media)
+    });
+    return arrayMaior
+}
+
+function insertAlunos() {
+    let notaT = 0
+    let nota = 0
+    alunoL.nome = leitor.question(`Ensira o nome do aluno: `)
+    for (let i = 0; i < 6; i++) {
+        nota = leitor.questionInt(`Ensira a nota do aluno de 1 a 10: `)
+        if (nota > 10) {
+            console.log('Valor excedido...\n');
+            i--
+        } else {
             notaT = notaT + nota
         }
-        alunoL[n].nota = notaT
-        let notaSoma = alunoL[n,2]
-        
-        alunoL[n].media = notaSoma / alunos
-        n++
+
     }
-    let value
-console.log(alunoL.findIndex(value === `nota`));
+    alunoL.nota = notaT
+    alunoL.media = notaT / 6
+    console.log(alunoL);
+    if (alunos.media >= 6) {
+        alunos.status = `Aprovado`
+    } else {
+        alunos.status = `Reprovado`
+    }
+    return alunoL
 }
 
 
