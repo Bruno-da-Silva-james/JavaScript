355f41ea9a6d462fed2e49162cdb99584b5f4c6c
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2023-11-11 19:47:56 -0300

    Create atv.js

---
+++ atividade11BrunoJames/atv.js
@@ -0,0 +1,11 @@
+const leitor = require('readline-sync')
+
+function atv1() {
+    let pessoa = {
+        nome ,
+        idade : 'Bruno' , 
+        profissao
+    }
+    console.log(this.pessoa = idade); 
+}
+atv1()
