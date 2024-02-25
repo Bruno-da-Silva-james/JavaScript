fe5bdf84c4f95040660cd0447df9e2f7c16f53f3
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2024-02-24 23:37:27 -0300

    att de ex

--- BeecrowdJavascript/1014.js
+++ BeecrowdJavascript/1014.js
@@ -1,13 +1,14 @@
-var input = require('fs').readFileSync('./tst.js', 'utf8')
-var lines = input.split('\n')
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 
 function consumptionCalc() {
     
-    let values1 = lines.shift();
+    let values1 = parseInt(lines.shift());
+    let values2 = parseFloat(lines.shift()).toFixed(1);
 
+    let consumption = (values1 / values2);
 
-    console.log(values1);
+    console.log(`${consumption.toFixed(3)} km/l`);
 }
 
-consumptionCalc()
-
+consumptionCalc();
---
+++ BeecrowdJavascript/1015.js
@@ -0,0 +1,20 @@
+var input = require('fs').readFileSync('./tst.js', 'utf8');
+var lines = input.split('\n');
+
+function distanceCalc() {
+    let values = lines.shift().split(' ');
+    let values1 = lines.shift().split(' ');
+
+    let x1 = parseFloat(values[0])
+    let y1 = parseFloat(values[1])
+
+    let x2 = parseFloat(values1[0])
+    let y2 = parseFloat(values1[1])
+
+    let formula = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+
+    console.log(formula.toFixed(4));
+
+}
+
+distanceCalc()
