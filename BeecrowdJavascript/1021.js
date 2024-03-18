972c32f4b7bf43f792105d97651a4c709797e277
Bruno da Silva James <136830934+Bruno-da-Silva-james@users.noreply.github.com>
2024-03-18 11:23:48 -0300

    errad

--- BeecrowdJavascript/1021.js
+++ BeecrowdJavascript/1021.js
@@ -10,24 +10,32 @@ function bank() {
     let dez = parseInt((((dinheiroTotal % 100) % 50) % 20) / 10)
     let cinco = parseInt(((((dinheiroTotal % 100) % 50) % 20) % 10) / 5)
     let dois = parseInt((((((dinheiroTotal % 100) % 50) % 20) % 10) % 5) / 2)
-    
+
+    let excludeReal = (dinheiroTotal - parseInt(dinheiroTotal)).toFixed(2)
+
     let um = parseInt(((((((dinheiroTotal % 100) % 50) % 20) % 10) % 5) % 2) / 1)
-    let cinquenta_cents = parseInt((((((((dinheiroTotal % 100) % 50) % 20) % 10) % 5) % 2) % 1) / 50)
-    let vinteCinco_cents
-    let dez_cents = 0
-    let cinco_cents
-    let um_cent
+    let cinquenta_cents = parseInt(excludeReal / .50)
+    let vinteCinco_cents = parseInt((excludeReal % .50) / .25)
+    let dez_cents = parseInt(((excludeReal % .50) % .25) / .10)
+    let cinco_cents = parseInt((((excludeReal % .50) % .25) % .10) / .05)
+    let um_cent = parseInt(((((excludeReal % .50) % .25) % .10) % .05) / .01)
 
-console.log(cinquenta_cents);
+    console.log(excludeReal);
 
-    console.log(dinheiroTotal);
-    console.log(`${cem} nota(s) de R$ 100,00`);
-    console.log(`${cinquenta} nota(s) de R$ 50,00`);
-    console.log(`${vinte} nota(s) de R$ 20,00`);
-    console.log(`${dez} nota(s) de R$ 10,00`);
-    console.log(`${cinco} nota(s) de R$ 5,00`);
-    console.log(`${dois} nota(s) de R$ 2,00`);
-    console.log(`${um} nota(s) de R$ 1,00`);
+    console.log("NOTAS:");
+    console.log(`${cem} nota(s) de R$ 100.00`);
+    console.log(`${cinquenta} nota(s) de R$ 50.00`);
+    console.log(`${vinte} nota(s) de R$ 20.00`);
+    console.log(`${dez} nota(s) de R$ 10.00`);
+    console.log(`${cinco} nota(s) de R$ 5.00`);
+    console.log(`${dois} nota(s) de R$ 2.00`);
+    console.log("MOEDAS:"); 
+    console.log(`${um} moeda(s) de R$ 1.00`);
+    console.log(`${cinquenta_cents} moeda(s) de R$ 0.50`);
+    console.log(`${vinteCinco_cents} moeda(s) de R$ 0.25`);
+    console.log(`${dez_cents} moeda(s) de R$ 0.10`);
+    console.log(`${cinco_cents} moeda(s) de R$ 0.5`);
+    console.log(`${um_cent} moeda(s) de R$ 0.01`);
 }
 
 bank()
